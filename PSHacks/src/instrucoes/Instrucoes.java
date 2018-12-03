3c5ceaa1144c67828c593f72a50aa70c8e7ba3c2
Micael Popping <mmpopping@inf.ufpel.edu.br>
2018-12-03 16:19:51 -0200

    att classe Instrucoes

--- PSHacks/src/instrucoes/Instrucoes.java
+++ PSHacks/src/instrucoes/Instrucoes.java
@@ -14,10 +14,36 @@ public class Instrucoes {
     
     /**
      * CONSTRUTOR.
+     * @author Micael Popping.
      */
     public Instrucoes(){
         
         setInstrucoesC(new InstrucoesC());
+    }
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    /**
+     * Analisa se a instrução é valida e qual o tipo.
+     * @param instrucao, instrução que deve ser analisada.
+     * @return uma stream contendo informações sobre a analise.
+     * @author Micael Popping.
+     */
+    public String analisarInstrucao(String instrucao){
+        
+        String analise;
+        
+        if(instrucao.charAt(0) == '@'){
+            
+            analise = "A";
+            analise+= instrucao.substring(1, instrucao.length());
+            
+            return analise;
+        }
+        
+        //FAZER
+        
+        return "XX";
     }
     
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
