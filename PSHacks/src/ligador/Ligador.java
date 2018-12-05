117084177a29513fb84ae7d6cd2bccf50febfb1e
Victor Barros <vhbarros@inf.ufpel.edu.br>
2018-12-05 03:25:02 -0200

    Add files via upload

--- PSHacks/src/ligador/Ligador.java
+++ PSHacks/src/ligador/Ligador.java
@@ -19,13 +19,29 @@ import java.util.Scanner;
  */
 public class Ligador {
     
+    public ArrayList <String> codigo;
+
     
     public Ligador(){
         
     }
     
     public void ler(String primeiroArquivo, String segundoArquivo) throws FileNotFoundException{
-        
+       
+            Scanner realizaLeitura = new Scanner(new FileReader(primeiroArquivo)).useDelimiter("\n");
+            Scanner realizaLeituraSegundo = new Scanner(new FileReader(segundoArquivo)).useDelimiter("\n");
+            
+            while(realizaLeitura.hasNext()){
+            
+                this.codigo.add(realizaLeitura.nextLine());
+                
+            }
+            
+            while(realizaLeituraSegundo.hasNext()){
+                
+                this.codigo.add(realizaLeituraSegundo.nextLine());
+                
+            }
     }
     
     public void pegarLinhas(String primeiraLinha, String segundaLinha)throws FileNotFoundException{
