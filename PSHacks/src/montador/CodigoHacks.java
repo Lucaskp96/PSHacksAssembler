47f897d781156511a681768582c46322b5e46bff
Micael Popping <mmpopping@inf.ufpel.edu.br>
2018-12-05 21:02:43 -0200

    Att classe CodigoHacks

--- PSHacks/src/montador/CodigoHacks.java
+++ PSHacks/src/montador/CodigoHacks.java
@@ -1,9 +1,40 @@
 package montador;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Micael Popping
  */
 public class CodigoHacks {
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    private ArrayList<String> codigoHacks;
+    private TabelaSimbolos tabelaSimbolos;
+ 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    CodigoHacks(){
+        
+        setCodigoHacks(new ArrayList<>());
+        setTabelaSimbolos(null);
+    }
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    private void setCodigoHacks(ArrayList<String> codigoHacks){
+        
+        this.codigoHacks = codigoHacks;
+    }
+    
+    public void setTabelaSimbolos(TabelaSimbolos tabelaSimbolos){
+        
+        this.tabelaSimbolos = tabelaSimbolos;
+    } 
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     
 }
