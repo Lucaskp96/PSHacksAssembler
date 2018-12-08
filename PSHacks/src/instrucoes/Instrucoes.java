32cff70094b609bb84caa7cb69951b923bfc7262
Micael Popping <mmpopping@inf.ufpel.edu.br>
2018-12-08 17:56:37 -0200

    Att classe Instrucoes

--- PSHacks/src/instrucoes/Instrucoes.java
+++ PSHacks/src/instrucoes/Instrucoes.java
@@ -1,5 +1,7 @@
 package instrucoes;
 
+import montador.Simbolo;
+
 /**
  * Classe que contem as instrucoes do hacks.
  * @author Micael Popping.
@@ -58,6 +60,90 @@ public class Instrucoes {
         }
         
         return null;
+    }
+    
+    /**
+     *
+     */
+    public String gerarLinhaBin(String linhaInstrucao, Simbolo simbolo){
+        
+        int endereco = simbolo.getEndereco();
+        
+        return gerarBinario(endereco) + " r";
+    }
+    
+    /**
+     *
+     */
+    public String gerarLinhaBin(String linhaInstrucao){
+        
+        int igual, pVirgula;
+        String resultado = "111", dest = null, comp = null, jump = null; 
+        
+        igual = linhaInstrucao.indexOf('=');
+        pVirgula = linhaInstrucao.indexOf(';');
+        
+        if((igual != -1) && (pVirgula == -1)){
+            
+            dest = linhaInstrucao.substring(0, igual);
+            comp = linhaInstrucao.substring(igual + 1, linhaInstrucao.length());
+            
+            resultado += instrucoesC.compBin(comp);
+            resultado += instrucoesC.destBin(dest);
+            resultado += instrucoesC.jumpBin("");
+            
+            return resultado + " a";
+        }else
+        if((igual == -1) && (pVirgula != -1)){
+            
+            comp = linhaInstrucao.substring(0, pVirgula);
+            jump = linhaInstrucao.substring(pVirgula + 1, linhaInstrucao.length()).trim();
+            
+            resultado += instrucoesC.compBin(comp);
+            resultado += instrucoesC.destBin("");
+            resultado += instrucoesC.jumpBin(jump);
+            
+            return resultado + " a";
+        }else
+        if((igual != -1) && (pVirgula != -1)){
+            
+            dest = linhaInstrucao.substring(0, igual);
+            comp = linhaInstrucao.substring(igual + 1, pVirgula);
+            jump = linhaInstrucao.substring(pVirgula + 1, linhaInstrucao.length()).trim();
+            
+            resultado += instrucoesC.compBin(comp);
+            resultado += instrucoesC.destBin(dest);
+            resultado += instrucoesC.jumpBin(jump);
+            
+            return resultado + " a";
+        }
+        
+       return null;
+    }
+    
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    /**
+     *
+     */
+    private String gerarBinario(int numero){
+        
+        String resultado = "";
+        int valorAtual = 0;
+        
+        for(int i = 15; i > -1; i--){
+            
+            if(valorAtual + Math.pow(2, i) <= numero){
+                
+                valorAtual += Math.pow(2, i);
+                resultado += "1";
+            }else{
+                
+                resultado += "0";
+            }
+        }
+        
+        return resultado;
     }
     
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
